e589c1b402af9d72d2b1eb51d5d47a4149886ab4
robuGdada <paudelronish@gmail.com>
2023-12-29 21:33:34 +0545

    header of create blog add

---
+++ src/components/headerBlog.tsx
@@ -0,0 +1,51 @@
+import {
+  Dimensions,
+  Image,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native";
+
+const { width: windowWidth } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#272727e6",
+    width: windowWidth,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 10,
+    paddingBottom: 20,
+  },
+  image: { width: 60, height: 60, borderRadius: 60 },
+  uploadImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+});
+
+export function HeaderBlog() {
+  return (
+    <Pressable style={styles.container}>
+      <View style={{ flexDirection: "row", gap: 20 }}>
+        <Image
+          source={require("../../assets/dummyImage.jpg")}
+          style={styles.image}
+        />
+        <TextInput
+          style={{ fontSize: 20, alignSelf: "center" }}
+          placeholder="Let's start crafting"
+          placeholderTextColor={"#888"}
+          editable={false}
+        ></TextInput>
+      </View>
+      <Image
+        source={require("../../assets/dummyImage.jpg")}
+        style={styles.uploadImage}
+      />
+    </Pressable>
+  );
+}
