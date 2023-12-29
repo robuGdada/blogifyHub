307330ab6092922de8162a6cae1ba485e6a995b8
robuGdada <paudelronish@gmail.com>
2023-12-29 21:33:21 +0545

    new header implemented

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,123 @@
-import { StyleSheet, Text, View } from "react-native";
-
-export function Header() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ color: "white" }}>hello </Text>
-      <Text style={{ color: "white" }}>hello </Text>
-    </View>
-  );
-}
+import React, { useState } from "react";
+import {
+  Image,
+  Keyboard,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "space-between",
+    backgroundColor: "#272727e6",
+  },
+  anotherContainer: {
     flexDirection: "row",
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingBottom: 15,
+    justifyContent: "space-between",
+  },
+  textWrapper: {
+    backgroundColor: "#141624",
+    borderRadius: 5,
+    marginLeft: "3%",
+    alignSelf: "center",
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+    padding: "2%",
+    fontSize: 16,
+  },
+  box: {
+    paddingLeft: "2%",
+  },
+  SearchContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  queryContainer: {
+    backgroundColor: "#888",
+    width: 300,
+    borderRadius: 5,
+    marginLeft: "4%",
+    flexDirection: "row",
+    paddingLeft: 30,
+    paddingRight: 15,
+    justifyContent: "center",
+  },
+  queryBox: {
+    width: "95%",
   },
 });
+
+export function Header() {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  function onPressSearchBar() {
+    setShowSearchBar(!showSearchBar);
+  }
+  function onPressCancel() {
+    setShowSearchBar(false);
+  }
+  function onPressClear() {
+    Keyboard.dismiss();
+  }
+
+  return (
+    <SafeAreaView style={styles.container} edges={["top"]}>
+      {showSearchBar ? (
+        <View style={styles.SearchContainer}>
+          <View style={styles.queryContainer}>
+            <TextInput
+              style={styles.queryBox}
+              placeholder="Search bar"
+              placeholderTextColor={"#141624"}
+            ></TextInput>
+            <TouchableOpacity onPress={onPressClear}>
+              <Image
+                source={require("../../assets/remove-icon.png")}
+                resizeMode="center"
+                style={{ width: 50, height: 50 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            onPress={onPressCancel}
+            style={{
+              width: 60,
+              padding: 3.5,
+              // backgroundColor: "white",
+            }}
+          >
+            <Text style={{ color: "white", alignSelf: "center" }}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={styles.anotherContainer}>
+          <View style={styles.box}>
+            <Pressable style={styles.textWrapper}>
+              <Text style={styles.text}>BlogifyHub</Text>
+            </Pressable>
+          </View>
+          <TouchableOpacity onPress={onPressSearchBar}>
+            <Image
+              source={require("../../assets/search.png")}
+              style={{
+                height: 35,
+                width: 40,
+                marginRight: "5%",
+                alignSelf: "center",
+                borderRadius: 4,
+                tintColor: "#8888",
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+}
