d1a1424d51aa8088d35b42013cea21f0634b0e79
robuGdada <paudelronish@gmail.com>
2023-12-30 15:01:31 +0545

    signin and signup added

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -13,10 +13,14 @@ import { Main } from "./main";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { Details } from "./detail";
 import { DetailHeader } from "./detailHeader";
+import { SignInForm } from "../auth/signIn";
+import { SignUpForm } from "../auth/signUp";
 
 export type RootStackParamList = {
   Main: NavigatorScreenParams<HomeTabParamList>;
   Details: Iblog;
+  signIn: undefined;
+  signUp: undefined;
 };
 
 export type HomeTabParamList = {
@@ -46,16 +50,27 @@ export function Navigator() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Home"
-          options={{ header: () => <Header /> }}
+          name="signIn"
+          options={{ headerShown: false }}
+          component={SignInForm}
+        />
+
+        <Stack.Screen
+          name="signUp"
+          component={SignUpForm}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Main"
           component={Main}
+          options={{ header: () => <Header /> }}
         />
         <Stack.Screen
           name="Details"
           component={Details}
-          options={{
-            header: () => <DetailHeader />,
-          }}
+          options={{ header: () => <DetailHeader /> }}
         />
       </Stack.Navigator>
     </NavigationContainer>
