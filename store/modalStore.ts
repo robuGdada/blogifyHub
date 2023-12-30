f618218d03fa1cc25c7b05a4e76881b606ec399b
robuGdada <paudelronish@gmail.com>
2023-12-30 15:00:22 +0545

    store expand

--- store/modalStore.ts
+++ store/modalStore.ts
@@ -3,6 +3,8 @@ import { proxy } from "valtio";
 interface IModalStore {
   loggedIn: boolean;
   setLoggedIn: () => void;
+
+  setLogOut: () => void;
   signUpModal: {
     open: boolean;
     setOpen: (val: boolean) => void;
@@ -12,10 +14,6 @@ interface IModalStore {
     setOpen: (val: boolean) => void;
   };
   logout: {
-    open: boolean;
-    setOpen: (val: boolean) => void;
-  };
-  forgotPassword: {
     open: boolean;
     setOpen: (val: boolean) => void;
   };
@@ -31,6 +29,10 @@ export const modalStore = proxy<IModalStore>({
       this.open = val;
     },
   },
+
+  setLogOut() {
+    this.loggedIn = false;
+  },
   signInModal: {
     open: false,
     setOpen(val) {
@@ -38,12 +40,6 @@ export const modalStore = proxy<IModalStore>({
     },
   },
   logout: {
-    open: false,
-    setOpen(val) {
-      this.open = val;
-    },
-  },
-  forgotPassword: {
     open: false,
     setOpen(val) {
       this.open = val;
